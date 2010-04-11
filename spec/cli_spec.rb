2048216e99b1966a6275e96fad312af1e1c7ef26
Mark Ryall <mark@ryall.name>
2010-04-11 16:00:13 +1000

    removed a completely useless file and fixed broken spec

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -4,8 +4,12 @@ require 'cardigan/cli'
 describe Cardigan::Cli do
   before do
     register_object :directory_class, Cardigan::Directory
+    register_object :root_context_class, Cardigan::RootContext
     stub_method :directory_class, :new
+    stub_method :root_context_class, :new
+    so_when(:directory_class).receives(:new).with('.cards').return(stub(:cards_directory))
     so_when(:directory_class).receives(:new).with('~').return(stub(:home_directory))
+    so_when(:root_context_class).receives(:new).return(stub(:root_context))
     so_when(:io, :as => :name_prompt).receives(:ask).with('Enter your full name').return('John Smith')
     so_when(:io, :as => :email_prompt).receives(:ask).with('Enter your email address').return('john@mail.com')
     @cli = Cardigan::Cli.new(stub(:io))
@@ -14,7 +18,7 @@ describe Cardigan::Cli do
   describe "when .cardigan file doesn't exist" do
     before do
       so_when(:home_directory, :as => :config_check).receives(:has_file?).with('.cardigan').return(false)
-      so_when(:home_directory, :as => :store_config).receives(:store).with({:name => 'John Smith',:email => 'john@mail.com'}).return({})
+      so_when(:home_directory, :as => :store_config).receives(:store).with('.cardigan', {:name => 'John Smith',:email => 'john@mail.com'}).return({})
       @cli.execute
     end
 
