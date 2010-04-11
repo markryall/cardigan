1ad7982bf745d3accb171915bbcf4e6fd2bd2d1d
Mark Ryall <mark@ryall.name>
2010-04-11 16:51:45 +1000

    added help and usage to claim and unclaim

--- lib/cardigan/command/claim_cards.rb
+++ lib/cardigan/command/claim_cards.rb
@@ -1,17 +1,21 @@
-module Cardigan
-  module Command
-    class ClaimCards
-      def initialize repository, io, name
-        @repository, @io, @name = repository, io, name
-      end
+class Cardigan::Command::ClaimCards
+  def initialize repository, io, name
+    @repository, @io, @name = repository, io, name
+  end
 
-      def execute numbers
-        @repository.each_card_from_indices(numbers) do |card|
-          @io.say "claiming \"#{card['name']}\""
-          card['owner'] = @name
-          @repository.save card
-        end
-      end
+  def execute numbers
+    @repository.each_card_from_indices(numbers) do |card|
+      @io.say "claiming \"#{card['name']}\""
+      card['owner'] = @name
+      @repository.save card
     end
+  end
+  
+  def usage
+    '<number> [<number]*'
+  end
+  
+  def help
+    'Sets you as the owner of the specified cards'
   end
 end
--- lib/cardigan/command/unclaim_cards.rb
+++ lib/cardigan/command/unclaim_cards.rb
@@ -1,17 +1,21 @@
-module Cardigan
-  module Command
-    class UnclaimCards
-      def initialize repository, io
-        @repository, @io = repository, io
-      end
+class Cardigan::Command::UnclaimCards
+  def initialize repository, io
+    @repository, @io = repository, io
+  end
 
-      def execute numbers
-        @repository.each_card_from_indices(numbers) do |card|
-          @io.say "unclaiming \"#{card['name']}\""
-          card.delete('owner')
-          @repository.save card
-        end
-      end
+  def execute numbers
+    @repository.each_card_from_indices(numbers) do |card|
+      @io.say "unclaiming \"#{card['name']}\""
+      card.delete('owner')
+      @repository.save card
     end
+  end
+
+  def usage
+    '<number> [<number]*'
+  end
+
+  def help
+    'Removes the owner of the specified cards'
   end
 end
