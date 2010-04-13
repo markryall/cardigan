00274f68c3fb42bf405f23bbf7dbc57eeb55241b
Mark Ryall <mark@ryall.name>
2010-04-14 00:00:08 +1000

    added some usage and help to change status and batch update

--- lib/cardigan/command/batch_update_cards.rb
+++ lib/cardigan/command/batch_update_cards.rb
@@ -3,8 +3,12 @@ require 'cardigan/card_editor'
 module Cardigan
   module Command
     class BatchUpdateCards
+      attr_reader :usage, :help
+
       def initialize repository, io
         @repository, @io = repository, io
+        @usage = '<field> [<index>]+'
+        @help = 'prompts for a new value for the specified field for the entries at the specified index'
       end
 
       def execute text
--- lib/cardigan/command/change_status.rb
+++ lib/cardigan/command/change_status.rb
@@ -1,18 +1,21 @@
 module Cardigan
   module Command
     class ChangeStatus
+      attr_reader :usage, :help
+
       def initialize entry, workflow_repository
-        @entry = entry
+        @entry, @workflow_repository = entry, workflow_repository
+        @usage = '<new status>'
+        @help = 'changes the status of the current entry'
+      end
+
+      def completion text
+        status = @entry['status'] || 'none'
+        @workflow_repository.load[status].grep(/^#{text}/).sort || []
       end
 
       def execute status
         @entry['status'] = status
-      end
-      
-      def completion
-        status = @entry['status'] || 'none'
-        next_statuses = @workflow_repository.load[status]
-        next_statuses ? next_statuses.map { |s| "now #{s}" } : []
       end
     end
   end
