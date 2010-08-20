53c782527cda6912c9b5bb769d25253beef4c99f
Mark Ryall <mark@ryall.name>
2010-08-21 09:34:08 +1000

    renamed workflow commands and deleted redundant create command

--- lib/cardigan/command/create_status.rb
+++
@@ -1,9 +0,0 @@
-class Cardigan::Command::CreateStatus
-  def initialize entry
-    @entry = entry
-  end
-
-  def execute key
-    @entry[key] ||= []
-  end
-end
--- lib/cardigan/workflow_context.rb
+++ lib/cardigan/workflow_context.rb
@@ -8,10 +8,9 @@ class Cardigan::WorkflowContext
     @io, @entry = io, entry
     @prompt_text = "#{File.expand_path('.').split('/').last.slice(0..0)}/workflow > "
     @commands = {
-      'list'               => Cardigan::Command.load(:show_entry, @entry, @io),
-      'create_status'      => Cardigan::Command.load(:create_status, @entry),
-      'add_transitions'    => Cardigan::Command.load(:add_transitions, @entry, @io),
-      'remove_transitions' => Cardigan::Command.load(:remove_transitions, @entry),
+      'ls'     => Cardigan::Command.load(:show_entry, @entry, @io),
+      'add'    => Cardigan::Command.load(:add_transitions, @entry, @io),
+      'rm'     => Cardigan::Command.load(:remove_transitions, @entry),
     }
   end
 end
