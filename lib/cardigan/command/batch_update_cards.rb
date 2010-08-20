93c3b2b5cdd1c737f43c120eca326c106a3eb6dd
Mark Ryall <mark@ryall.name>
2010-08-20 21:00:51 +1000

    added spec, usage and help to batch update command

--- lib/cardigan/command/batch_update_cards.rb
+++ lib/cardigan/command/batch_update_cards.rb
@@ -1,24 +1,21 @@
+require 'cardigan/commands'
 require 'cardigan/card_editor'
 
-module Cardigan
-  module Command
-    class BatchUpdateCards
-      attr_reader :usage, :help
+class Cardigan::Command::BatchUpdateCards
+  attr_reader :usage, :help
 
-      def initialize repository, io
-        @repository, @io = repository, io
-        @usage = '<field> [<index>]+'
-        @help = 'prompts for a new value for the specified field for the entries at the specified index'
-      end
+  def initialize repository, io
+    @repository, @io = repository, io
+    @usage = '<field> <number>*'
+    @help = 'Sets the specified field to a new value for the specified cards (by index in the list)'
+  end
 
-      def execute text
-        key, *rest = text.scan(/\w+/)
-        value = @io.ask("Enter the new value for #{key}")
-        @repository.each_card_from_indices(rest.join(' ')) do |card|
-          Cardigan::CardEditor.new(card, @io).set key, value
-          @repository.save card
-        end
-      end
+  def execute text
+    key, *rest = text.scan(/\w+/)
+    value = @io.ask("Enter the new value for #{key}")
+    @repository.each_card_from_indices(rest.join(' ')) do |card|
+      Cardigan::CardEditor.new(card, @io).set key, value
+      @repository[card.id] = card
     end
   end
 end
---
+++ spec/command/batch_update_cards_spec.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__)+'/../spec_helper'
+require 'cardigan/command/batch_update_cards'
+
+describe Cardigan::Command::BatchUpdateCards do
+  extend CommandSpec
+  with_usage '<field> <number>*'
+  with_help 'Sets the specified field to a new value for the specified cards (by index in the list)'
+
+  before { @command = Cardigan::Command::BatchUpdateCards.new(nil,nil) }
+end
